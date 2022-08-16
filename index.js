7dc4c4c252cca30ce5dcce14b94cc21a3db2bd6d
derletzte255 <genii.mk@yandex.ru>
2022-08-16 20:45:18 +0300

    prep for heroku

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 persons = [
 	{
@@ -31,6 +32,7 @@ const generateId = () => {
 	return Math.floor(Math.random() * 10 ** 12)
 }
 
+app.use(cors())
 app.use(express.json())
 app.use(
 	morgan((tokens, req, res) => {
