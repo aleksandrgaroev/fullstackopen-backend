9152a79bbd5a8abbb0c170af1ef8b03aee2c306c
derletzte255 <genii.mk@yandex.ru>
2022-08-26 19:11:08 +0300

    fixed eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,6 @@ module.exports = {
 		'no-trailing-spaces': 'error',
 		'object-curly-spacing': ['error', 'always'],
 		'arrow-spacing': ['error', { before: true, after: true }],
+		'no-console': 0,
 	},
 }
